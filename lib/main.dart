5124310bfeefa52151741f20949539fa34113610
Lohot <bintangursiboro@gmail.com>
2019-10-22 11:53:18 +0700

    adding firebase crashlytics to app

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:basic_flutter_app/page/main/bloc/main_page_bloc.dart';
 import 'package:basic_flutter_app/page/main/main_page.dart';
 import 'package:basic_flutter_app/utility/database/database_helper.dart';
 import 'package:basic_flutter_app/utility/injector/injector.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  Crashlytics.instance.enableInDevMode = true;
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
   runApp(InitApp());
 }
 
@@ -23,8 +26,45 @@ class InitApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Basic SQFlite',
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter Basic App'),
+          appBar: PreferredSize(
+            preferredSize: Size(null, 100),
+            child: Container(
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(color: Colors.black12, spreadRadius: 5, blurRadius: 2)
+              ]),
+              height: 100,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15),
+                ),
+                child: Container(
+                  color: Colors.blueAccent,
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.navigate_before,
+                          size: 40,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "Basic App",
+                          style: TextStyle(fontSize: 30, color: Colors.white),
+                        ),
+                        Icon(
+                          Icons.navigate_before,
+                          color: Colors.transparent,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
           body: BlocProvider<MainPageBloc>(
             builder: (context) => MainPageBloc(),
